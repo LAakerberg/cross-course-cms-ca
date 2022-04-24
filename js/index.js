be504bb641e131381907eec98c008dc85d2a8760
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-04-24 19:31:49 +0200

    Big update with several functions

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ const contentOne = document.querySelector(".one");
 const contentTwo = document.querySelector(".two");
 const contentThree = document.querySelector(".three");
 
+const link = "https://onedesign.nu/cross-course-cms-ca/wp-json/wc/v3/products";
+// API call to secure site that's need key access
+const link2 =
+  "https://onedesign.nu/cross-course-cms-ca/wp-json/wc/store/products";
+// API call to unsecure and public site
+const key =
+  "?consumer_key=ck_b08e1b35330f9429c3dd3c0217524fc5f043f0a9&consumer_secret=cs_34ca13e5e0845f327dd9ee677ab696929a186125";
+// Security key
+const url = link + key;
+// combine the link + key
 contentOne.innerHTML += `
 <div class="center">
 <div class="campaign-left"><img class="jackets__image" src="images/andreas-wagner-8H5kNkYmJYQ-unsplash-width1000.jpg"/>
@@ -9,9 +19,6 @@ contentOne.innerHTML += `
 <div class="campaign-right"><img class="adventure__image" src="images/nina-luong-dy4QG5c5xWQ-unsplash-width1000.jpg" />
 <a href="index.html" class="top-text">Your next adventure</a></div>
 </div>`;
-
-const url =
-  "https://onedesign.nu/cross-course-cms-ca/wp-json/wc/store/products";
 
 async function getProducts() {
   try {
@@ -29,14 +36,20 @@ getProducts();
 
 function createHTML(products) {
   products.forEach(function (product) {
-    contentTwo.innerHTML += `
-    <div class="product">
-    <img src="${product.images[0].src}" alt="${product.name}" class="products__image" />
-    <div class="small-product-text">
-    <h3>${product.name}</h3>
-    <p>${product.short_description}</p>
-    <p class="price">${product.prices.regular_price},- NOK</p>
-    </div></div>
-    `;
+    const featured = product.featured;
+
+    if (featured === true) {
+      contentTwo.innerHTML += `
+      <div class="product">
+      <img src="${product.images[0].src}" alt="${product.name}" class="products__image" />
+      <div class="small-product-text">
+      <h3>${product.name}</h3>
+      <p>${product.attributes[0].name} - ${product.attributes[0].options}</p>
+      <p class="price">${product.regular_price},- NOK</p>
+      </div></div>
+      `;
+    } else {
+      console.log("Error");
+    }
   });
 }
--- js/jackets.js
+++ js/jackets.js
@@ -1,13 +1,19 @@
 const contentJacket = document.querySelector(".jacket-list");
 const loadingBar = document.querySelector(".load");
+const categoryCheck = document.querySelectorAll(".category");
+const perPage = document.querySelector(".per-page");
 
 const link = "https://onedesign.nu/cross-course-cms-ca/wp-json/wc/v3/products";
+// API call to secure site that's need key access
+const link2 =
+  "https://onedesign.nu/cross-course-cms-ca/wp-json/wc/store/products";
+// API call to unsecure and public site
 
 const key =
   "?consumer_key=ck_b08e1b35330f9429c3dd3c0217524fc5f043f0a9&consumer_secret=cs_34ca13e5e0845f327dd9ee677ab696929a186125";
-
+// Security key
 const url = link + key;
-
+// combine the link + key
 loadingBar.innerHTML += `<div class="loading"><p class="loading-text">Loading...</p><div class="loader"></div></div>`;
 contentJacket.innerHTML = ``;
 
@@ -15,7 +21,21 @@ async function getProducts() {
   try {
     const response = await fetch(url);
     const getResults = await response.json();
-    createHTML(getResults);
+    const products = getResults;
+    products.forEach(function (product) {
+      loadingBar.innerHTML = ``;
+      contentJacket.innerHTML += `
+      <a href="jackets-specific.html?id=${product.id}">
+      <div class="product">
+      <img src="${product.images[0].src}" alt="${product.name}" class="products__image" />
+      <div class="small-product-text">
+      <h3>${product.name}</h3>
+      <p>${product.attributes[0].name} - ${product.attributes[0].options}</p>
+      <p class="price">${product.regular_price},- NOK</p>
+      </div></div>
+      </a>
+      `;
+    });
 
     console.log(getResults);
   } catch (error) {
@@ -23,21 +43,29 @@ async function getProducts() {
   }
 }
 
-getProducts();
+getProducts(link);
+// call the function/API
 
-function createHTML(products) {
-  products.forEach(function (product) {
-    loadingBar.innerHTML = ``;
-    contentJacket.innerHTML += `
-    <a href="jackets-specific.html?id=${product.id}">
-    <div class="product">
-    <img src="${product.images[0].src}" alt="${product.name}" class="products__image" />
-    <div class="small-product-text">
-    <h3>${product.name}</h3>
-    <p>${product.attributes[0].name} - ${product.attributes[0].options}</p>
-    <p class="price">${product.regular_price},- NOK</p>
-    </div></div>
-    </a>
-    `;
-  });
-}
+/*
+perPage.onchange = function (event) {
+  const newUrl = link + `per_page=${event.target.value}` + key;
+  loadingBar.innerHTML = ``;
+  contentJacket.innerHTML = ``;
+  getProducts(newUrl);
+};
+*/
+
+categoryCheck.forEach(function (category) {
+  category.onclick = function (event) {
+    let newUrl;
+    if (event.target.id === "popular") {
+      newUrl = link + "?featured=true";
+      contentJacket.innerHTML += ``;
+    } else {
+      const categorySpecific = event.target.value;
+      newUrl = link + `/categories/${categorySpecific}` + key;
+    }
+    contentJacket.innerHTML += ``;
+    getProducts(newUrl);
+  };
+});
